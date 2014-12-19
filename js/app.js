d34e1e8db238c835db38fd07a7b4b60f5787b7a4
janocl <janocl84@gmail.com>
2014-12-19 01:09:46 -0300

    agregando las directivas de angular en el index.html

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ app.controller('HomeViewController', ['$scope', function($scope){
 // Controlador donde esta el listado de elementos y se define un arreglo expenses
 app.controller('ExpensesViewController', ['$scope', function($scope){
 	$scope.expenses = [
-		{description: 'food', amount: 10, date: '2014-10-01'},
-		{description: 'tickets', amount: 11, date: '2014-10-02'},
-		{description: 'food', amount: 12, date: '2014-10-03'},
-		{description: 'phone credit', amount: 13, date: '2014-10-04'},
-		{description: 'bills', amount: 14, date: '2014-10-05'},
-		{description: 'food', amount: 15, date: '2014-10-06'},
+		{description: 'food', amount: 10, date: '2014-12-01'},
+		{description: 'tickets', amount: 11, date: '2014-12-02'},
+		{description: 'food', amount: 12, date: '2014-12-03'},
+		{description: 'phone credit', amount: 13, date: '2014-12-04'},
+		{description: 'bills', amount: 14, date: '2014-12-05'},
+		{description: 'food', amount: 15, date: '2014-12-06'},
 	]
 }])
