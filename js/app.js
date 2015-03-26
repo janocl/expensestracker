08a2f12641281aaeaa51f2b84fbbf3ff216b5a1f
janocl <janocl84@gmail.com>
2015-03-25 21:14:44 -0300

    Agregado Servicio en el controlador

--- js/app.js
+++ js/app.js
@@ -7,15 +7,8 @@ app.controller('HomeViewController', ['$scope', function($scope){
 }]);
 
 // Controlador donde esta el listado de elementos y se define un arreglo expenses
-app.controller('ExpensesViewController', ['$scope', function($scope){
-	$scope.expenses = [
-		{description: 'food', amount: 10, date: '2014-12-01'},
-		{description: 'tickets', amount: 11, date: '2014-12-02'},
-		{description: 'food', amount: 12, date: '2014-12-03'},
-		{description: 'phone credit', amount: 13, date: '2014-12-04'},
-		{description: 'bills', amount: 14, date: '2014-12-05'},
-		{description: 'food', amount: 15, date: '2014-12-06'},
-	]
+app.controller('ExpensesViewController', ['$scope', 'Expenses',function($scope, Expenses){
+	$scope.expenses = Expenses.entries;
 }]);
 
 app.controller('ExpenseViewController', ['$scope', function($scope){
@@ -26,5 +19,23 @@ app.config(['$routeProvider', function($routeProvider){
 	$routeProvider
 		.when('/', { templateUrl: 'views/expenses.html', controller: 'ExpensesViewController' })
 		.when('/expenses', { templateUrl: 'views/expenses.html', controller: 'ExpensesViewController' })
+		.when('/expenses/new', { templateUrl: 'views/form.html', controller: 'ExpensesViewController' })
 		.otherwise({redirectTo: '/'});
 }]);
+
+//Servicio
+app.factory('Expenses', function(){
+
+	var service = {};
+
+	service.entries = [
+		{description: 'food', amount: 10, date: '2014-12-01'},
+		{description: 'tickets', amount: 11, date: '2014-12-02'},
+		{description: 'food', amount: 12, date: '2014-12-03'},
+		{description: 'phone credit', amount: 13, date: '2014-12-04'},
+		{description: 'bills', amount: 14, date: '2014-12-05'},
+		{description: 'food', amount: 15, date: '2014-12-06'},
+	];
+
+	return service;
+})
