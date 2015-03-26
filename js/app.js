635a13884c2c791e3b8cb57c16c309253b1704a7
janocl <janocl84@gmail.com>
2015-03-26 00:49:10 -0300

    Arreglada la ruta para un nuevo gasto

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 // Nuevo modulo
-var app = angular.module('expensesApp', ['ngRoute']);
+var app = angular.module('expensesApp', ['ngRoute'])
 
-// Controlador principal y le pasamos el ambito $scope
-app.controller('HomeViewController', ['$scope', function($scope){
-	$scope.appTitle = 'Expenses Tracker';
-}]);
 
 //Define las rutas para la app, y cada ruta define un template y un controlador.
 app.config(['$routeProvider', function($routeProvider){
@@ -19,7 +15,7 @@ app.config(['$routeProvider', function($routeProvider){
 			})
 		.when('/expenses/new', {
 			templateUrl : 'views/expenseForm.html',
-			controller  : 'ExpensesViewController'
+			controller  : 'ExpenseViewController'
 			})
 		.when('/expenses/edit/:id', {
 			templateUrl : 'views/expenseForm.html',
@@ -36,25 +32,33 @@ app.factory('Expenses', function(){
 	var service = {};
 
 	service.entries = [
-		{description: 'food', amount: 10, date: '2014-12-01'},
-		{description: 'tickets', amount: 11, date: '2014-12-02'},
-		{description: 'food', amount: 12, date: '2014-12-03'},
-		{description: 'phone credit', amount: 13, date: '2014-12-04'},
-		{description: 'bills', amount: 14, date: '2014-12-05'},
-		{description: 'food', amount: 15, date: '2014-12-06'},
+		{id:1, description: 'food', amount: 10, date: '2014-12-01'},
+		{id:2, description: 'tickets', amount: 11, date: '2014-12-02'},
+		{id:3, description: 'food', amount: 12, date: '2014-12-03'},
+		{id:4, description: 'phone credit', amount: 13, date: '2014-12-04'},
+		{id:5, description: 'bills', amount: 14, date: '2014-12-05'},
+		{id:6, description: 'food', amount: 15, date: '2014-12-06'},
 	];
 
+	service.save = function(entry) {
+		service.entries.push(entry);
+	}
+
 	return service;
-})
+});
 
 // Controlador donde esta el listado de elementos y se define un arreglo expenses
-app.controller('ExpensesViewController', ['$scope', 'Expenses', function($scope, Expenses){
-	$scope.expenses = Expenses.entries;
+app.controller('ExpensesViewController', ['$scope', 'Expenses', function($scope, Expenses) {
+  $scope.expenses = Expenses.entries;
 }]);
 
-app.controller('ExpenseViewController', ['$scope', '$routeParams', '$location','Expenses', function($scope, $routeParams, $location, Expenses){
-	//Si no viene un parametro 'id'
-	if (!$routeParams.id) {
-		$scope.expense = {id: 7, description: 'something', amount: 20, date: new Date()};
-	}
+app.controller('ExpenseViewController', ['$scope', '$routeParams', '$location', 'Expenses', function($scope, $routeParams, $location, Expenses) {
+  if(!$routeParams.id) {
+    $scope.expense = {id: 7, description: 'something', amount: 10, date: new Date()};
+  }
+	
+	$scope.save = function() {
+    Expenses.save($scope.expense);
+    $location.path('/');
+  }
 }]);
